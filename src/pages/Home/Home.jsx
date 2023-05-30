378fc417e7d4dd0fc4b481f8e7dd3ea87d0a31cd
unknown <oakhmadjonov1@gmail.com>
2023-05-30 14:30:52 +0500

    Banner frame added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Home.scss";
+import Banner from "./components/Banner/Banner";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home Page</h1>
+      <Banner />
     </div>
   );
 };
---
+++ src/pages/Home/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.scss";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner__container">
+        <div className="banner__card"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
