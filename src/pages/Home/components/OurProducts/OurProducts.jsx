3adc8957f6984e7cfaf1d132e51ad3890772b0ff
unknown <oakhmadjonov1@gmail.com>
2023-06-02 10:47:55 +0500

    All products and Trending compontns's title and subtitle added

--- src/pages/Home/components/OurProducts/OurProducts.jsx
+++ src/pages/Home/components/OurProducts/OurProducts.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import "./OurProducts.scss";
+import { Link } from "react-router-dom";
 const OurProducts = () => {
   return (
     <div className="ourproducts">
-      <div className="ourproducts__container"></div>
+      <div className="ourproducts__container">
+        <div className="ourproducts__title">
+          <h3>OUR PRODUCTS</h3>
+        </div>
+        <div className="ourproducts__sub__titles">
+          <Link className="allproducts op__item">All Products</Link>
+          <Link className="bestsellers op__item">Best Sellers</Link>
+          <Link className="newarrivals op__item">New Arrivals</Link>
+          <Link className="todaysdeals op__item">Todays Deals</Link>
+        </div>
+
+        <div className="ourproducts__content-wrapper">
+          <div className="ourproducts__content"></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home/components/Trending/Trending.jsx
+++ src/pages/Home/components/Trending/Trending.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./Trending.scss";
+import { Link } from "react-router-dom";
 const Trending = () => {
   return (
     <div className="trending">
-      <div className="trending__container"></div>
+      <div className="trending__container">
+        <div className="ternding__title">
+          <h3>TRENDING</h3>
+        </div>
+        <div className="sub__product__titles">
+          <Link className="topproducts trend__item">Top Products</Link>
+          <Link className="newarrivals trend__item">New Arrivals</Link>
+          <Link className="bestsellers trend__item">Best Sellers</Link>
+        </div>
+        <div className="trending__content-wrapper">
+          <div className="trending__content"></div>
+        </div>
+      </div>
     </div>
   );
 };
