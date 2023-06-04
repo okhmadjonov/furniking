faf957177b286c3bc47b51aac57550a69df11e7c
unknown <oakhmadjonov1@gmail.com>
2023-06-04 10:53:35 +0500

    Trending all design almost  done

--- src/pages/Home/components/Trending/TopProducts/TopProducts.jsx
+++ src/pages/Home/components/Trending/TopProducts/TopProducts.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import "./TopProducts.scss";
+import { BsStar, BsHeart, BsBagPlus, BsEye } from "react-icons/bs";
+import { RxUpdate } from "react-icons/rx";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllTrends } from "../../../../../reduxToolkit/products/extraReducer";
 const TopProducts = () => {
@@ -45,12 +47,44 @@ export const Products = () => {
             className="product__img"
             style={{
               backgroundImage: `url(${item.imgUrl})`,
-            }}></div>
-          {/* <div className="product__type"></div> */}
+            }}>
+            <div className="product__prioritet">
+              <p className="prioritet">{item.prioritet}</p>
+            </div>
+            <div className="product__hover__items">
+              <div className="hover__item heart">
+                <BsHeart />
+              </div>
+              <div className="hover__item bag">
+                <BsBagPlus />
+              </div>
+              <div className="hover__item update">
+                <RxUpdate />
+              </div>
+
+              <div className="hover__item eye">
+                <BsEye />
+              </div>
+            </div>
+          </div>
+
           <div className="product__name">
+            <p>{item.type}</p>
             <h3>{item.name}</h3>
           </div>
-          <div className="product__price__stars"></div>
+          <div className="product__price__stars">
+            <div className="new__old__price">
+              <p className="new_p">${item.new_price}</p>
+              <p className="old_p">${item.old_price}</p>
+            </div>
+            <div className="stars">
+              <BsStar />
+              <BsStar />
+              <BsStar />
+              <BsStar />
+              <BsStar />
+            </div>
+          </div>
         </div>
       ))}
     </div>
